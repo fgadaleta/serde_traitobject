af6985d7d35fd59c67ae14df5c19c87167850c33
Alec Mocatta <alec@mocatta.net>
2025-03-19 17:05:29 +0000

    v0.2.9

--- src/lib.rs
+++ src/lib.rs
@@ -99,7 +99,7 @@
 //!
 //! This crate currently requires Rust nightly.
 
-#![doc(html_root_url = "https://docs.rs/serde_traitobject/0.2.7")]
+#![doc(html_root_url = "https://docs.rs/serde_traitobject/0.2.9")]
 #![feature(
 	arbitrary_self_types_pointers,
 	coerce_unsized,
